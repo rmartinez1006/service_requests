9588589726f18b05f14fd872dee1af598eff5f3c
Roberto Martinez <rmartinez1006@prodigy.net.mx>
2010-08-28 00:09:01 -0500

    Agosto 28 Actualización

--- app/models/administration/user.rb
+++ app/models/administration/user.rb
@@ -1,8 +1,8 @@
 class Administration::User < ActiveRecord::Base
 
   validates :role_id, :presence => true
+  validates :ubication_id, :presence => true
   belongs_to :user_role, :class_name => 'Administration::User_role'
-
-
+  belongs_to :ubication, :class_name => 'Catalogs::Ubication'
 
 end
