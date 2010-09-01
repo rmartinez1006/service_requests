886f51d3ae762d083b4a30ff4dae2c301287ed82
Roberto Martinez <rmartinez1006@prodigy.net.mx>
2010-08-31 23:37:58 -0500

    Agosto 31, 11:38pm Actualización

--- app/models/administration/user_hierachy.rb
+++ app/models/administration/user_hierachy.rb
@@ -1,3 +1,4 @@
 class Administration::UserHierachy < ActiveRecord::Base
-  belongs_to :user, :foreign_key => "user_id"
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :userhelper, :class_name => "User", :foreign_key => "helper_id"
 end
