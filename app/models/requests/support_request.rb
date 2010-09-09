40ff44c12649574c4bc58817892c8950a96b3466
Roberto Martinez <rmartinez1006@prodigy.net.mx>
2010-09-09 00:14:12 -0500

    Sep 9 Estuve trabajando con Solicitudes de Servicio

--- app/models/requests/support_request.rb
+++ app/models/requests/support_request.rb
@@ -1,5 +1,40 @@
 class Requests::SupportRequest < ActiveRecord::Base
 
   belongs_to :ubication, :class_name => 'Catalogs::Ubication'
-  
+  belongs_to :userhelper, :class_name => "Administration::User", :foreign_key => "helper_id"
+  belongs_to :requests_status, :class_name => "Catalogs::RequestStatus", :foreign_key => "status_id"
+
+
+  def before_save
+    if  self.request_no == nil
+      self.request_no = request_number
+    end
+    if self.status_id == nil
+      self.status_id= 1
+      self.status_chng_date=Time.now
+    end
+  end
+
+  def description_left(l)
+    r=self.description
+    if r.length > l
+      r= r.to(l-3)+'...'
+    end
+    r
+  end
+
+  def atiende
+    
+
+    if self.helper_id == nil
+       r = '...'
+    else
+      r=self.userhelper.name
+    end          
+    r
+  end
+
+  def request_number
+    return Time.now
+  end
 end
