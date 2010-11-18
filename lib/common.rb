c5397ead949ec7f5a50c940737b0e449c4a8182f
Ramón Martínez Olvera <martinezo@developer-vi.(none)>
2010-11-18 08:33:12 -0600

    Adding common fuctions library

---
+++ lib/common.rb
@@ -0,0 +1,14 @@
+module Common
+
+   #Para usar las funciones definidas en este modulo dentro de una clase
+   #solo hay que agregar la sentencia:
+   #
+   #include Common
+   #
+   #dentro de la clase
+
+   def test()
+      #funcion de ejemplo uso comun
+   end
+    
+end
