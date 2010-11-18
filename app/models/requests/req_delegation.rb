62366d5c928d4e1bc04c83503bbf18b11c38d9c2
Ramón Martínez Olvera <martinezo@developer-vi.(none)>
2010-11-18 08:02:07 -0600

    201011180805 remove MVC of request

--- app/models/requests/req_delegation.rb
+++
@@ -1,6 +0,0 @@
-class Requests::ReqDelegation < ActiveRecord::Base
-   belongs_to :user, :class_name => "Administration::User", :foreign_key => "user_id"
-   belongs_to :userhelper, :class_name => "Administration::User", :foreign_key => "helper_id"
-   belongs_to :request, :class_name => "Requests::SupportRequest", :foreign_key => "request_id"
-
-end
--- app/models/requests/request_commentary.rb
+++
@@ -1,4 +0,0 @@
-class Requests::RequestCommentary < ActiveRecord::Base
-  belongs_to :request, :class_name => "Requests::SupportRequest", :foreign_key => "request_id"
-  belongs_to :user, :class_name => "Administration::User", :foreign_key => "user_id"
-end
